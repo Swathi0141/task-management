b98b1fc419f5f58396efe12c173dceec45dfd841
Swathi0141 <90282618+Swathi0141@users.noreply.github.com>
2025-04-22 00:03:55 -0500

    Created Google Authentication and Employee Serializers

--- task_management/tasks/serializers.py
+++ task_management/tasks/serializers.py
@@ -1,5 +1,40 @@
 from rest_framework import serializers
 from .models import Project, Task, TaskStatus, Comment, Attachment
+from django.contrib.auth.models import User
+from google.auth.transport import requests
+from google.oauth2 import id_token
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class GoogleAuthSerializer(serializers.Serializer):
+    token = serializers.CharField()
+
+    def validate(self, attrs):
+        token = attrs.get("token")
+        try:
+            idinfo = id_token.verify_oauth2_token(token, requests.Request())
+
+            email = idinfo.get("email")
+            name = idinfo.get("name")
+
+            if not email:
+                raise serializers.ValidationError("Email not available")
+
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                raise serializers.ValidationError("You are not authorized to log in.")
+
+            refresh = RefreshToken.for_user(user)
+            return {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+                "user": {
+                    "email": user.email,
+                    "name": user.first_name,
+                },
+            }
+        except ValueError:
+            raise serializers.ValidationError("Invalid token")
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,3 +60,13 @@ class AttachmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attachment
         fields = ['file']
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ['id', 'full_name', 'email']
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
